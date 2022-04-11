336643dc7f74fb457e9e07df6420e0bb7fb6ac93
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-04-12 00:05:31 +0600

    fix linter errors in styles.css and script.js file

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let modal = document.getElementById("modal");
-let btn = document.getElementById("modal-btn");
-let quit = document.getElementsByClassName("close")[0];
-let nav = document.getElementById("nav");
-let container = document.getElementById("h-container");
-let mContainer = document.getElementById("m-container")
-let disappear1 = document.getElementById("menu-link1");
-let disappear2 = document.getElementById("menu-link2");
-let disappear3 = document.getElementById("menu-link3");
+const modal = document.getElementById('modal');
+const btn = document.getElementById('modal-btn');
+const quit = document.getElementsByClassName('close')[0];
+let nav = document.getElementById('nav');
+let container = document.getElementById('h-container');
+let mContainer = document.getElementById('m-container');
+let disappear1 = document.getElementById('menu-link1');
+let disappear2 = document.getElementById('menu-link2');
+let disappear3 = document.getElementById('menu-link3');
 let disappear = [disappear1, disappear2, disappear3];
 
 btn.onclick = () => {
@@ -14,21 +14,20 @@ btn.onclick = () => {
   nav.style.filter = "blur(8px)";
   container.style.filter = "blur(8px)";
   mContainer.style.filter = "blur(8px)";
-}
+};
 
 quit.onclick = () => {
   modal.style.display = "none";
   nav.style.filter = "none";
   container.style.filter = "none";
   mContainer.style.filter = "none";
-}
+};
 
-for (let i = 0; i < disappear.length; i++) {
+for (let i = 0; i < disappear.length; i += 1) {
   disappear[i].onclick = () => {
     modal.style.display = "none";
     nav.style.filter = "none";
     container.style.filter = "none";
     mContainer.style.filter = "none";
-  }
-}
-
+  };
+};
