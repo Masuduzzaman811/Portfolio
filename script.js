758877fdc3f6c4e5f853d0d9dd21cf550c3a592d
wale-prog <olapetanwale@outlook.com>
2022-04-11 17:58:19 +0100

    Add modal disappearance to mobile menu when clicked

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let mContainer = document.getElementById("m-container")
 let disappear1 = document.getElementById("menu-link1");
 let disappear2 = document.getElementById("menu-link2");
 let disappear3 = document.getElementById("menu-link3");
+let disappear = [disappear1, disappear2, disappear3];
 
 btn.onclick = () => {
   modal.style.display = "block";
@@ -22,10 +23,12 @@ quit.onclick = () => {
   mContainer.style.filter = "none";
 }
 
-disappear1.onclick = () => {
-  modal.style.display = "none";
-  nav.style.filter = "none";
-  container.style.filter = "none";
-  mContainer.style.filter = "none";
+for (let i = 0; i < disappear.length; i++) {
+  disappear[i].onclick = () => {
+    modal.style.display = "none";
+    nav.style.filter = "none";
+    container.style.filter = "none";
+    mContainer.style.filter = "none";
+  }
 }
 
