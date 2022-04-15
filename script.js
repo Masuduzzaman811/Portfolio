7d3e4880889cee19dadfe2b048777547a1ed50af
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-04-15 16:23:17 +0600

    fix linter errors in css

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const disappear1 = document.getElementById('menu-link1');
 const disappear2 = document.getElementById('menu-link2');
 const disappear3 = document.getElementById('menu-link3');
 const disappear = [disappear1, disappear2, disappear3];
-const card = document.querySelector(".work-card");
 
 btn.onclick = () => {
   modalm.style.display = 'block';
@@ -40,7 +39,7 @@ const PROJECTS_LIST = [{
   description: '<li class="card-list-first-child card-list-item">CANOPY</li> <li class="card-list-item">Back End Dev</li> <li class="card-list-item">2015</li>',
   Image: 'images/work-card1.svg',
   project_txt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  technologies: ['HTML', 'CSS', 'JavaScript'],
+  technologies: ['HTML','CSS', 'JavaScript'],
   live_btn: '#',
   source_btn: '#'
 },
@@ -49,7 +48,7 @@ const PROJECTS_LIST = [{
   description: '<li class="card-list-first-child card-list-item">CANOPY</li> <li class="card-list-item">Front End Dev</li> <li class="card-list-item">2016</li>',
   Image: 'images/work-card2.svg',
   project_txt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  technologies: ['HTML', 'CSS', 'JavaScript'],
+  technologies: ['HTML','CSS', 'JavaScript'],
   live_btn: '#',
   source_btn: '#'
 },
@@ -58,16 +57,16 @@ const PROJECTS_LIST = [{
   description: '<li class="card-list-first-child card-list-item">CANOPY</li> <li class="card-list-item">Front End Dev</li> <li class="card-list-item">2019</li> ',
   Image: 'images/work-card3.svg',
   project_txt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  technologies: ['HTML', 'CSS', 'JavaScript'],
+  technologies: ['HTML','CSS', 'JavaScript'],
   live_btn: '#',
   source_btn: '#'
 },
 {
   name: 'Multi-Post Stories',
-  description: '<li class="card-list-first-child card-list-item">CANOPY</li> <li class="card-list-item">Front End Dev</li> <li class="card-list-item">2019</li> ',
+  description: '<li class="card-list-first-child card-list-item">CANOPY</li> <li class="card-list-item">Front End Dev</li> <li class="card-list-item">2019</li> ', 
   Image: 'images/work-card4.svg',
   project_txt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  technologies: ['HTML', 'CSS', 'Ruby'],
+  technologies: ['HTML','CSS', 'Ruby'],
   live_btn: '#',
   source_btn: '#'
 }];
@@ -77,7 +76,7 @@ const SEE_PROJECT2 = document.getElementById('see-btn2');
 const SEE_PROJECT3 = document.getElementById('see-btn3');
 const SEE_PROJECT4 = document.getElementById('see-btn4');
 
-SEE_PROJECT1.onclick = () => {
+SEE_PROJECT1.onclick = ()=> {
   const p1_name = document.getElementById('p1-name');
   p1_name.innerHTML = PROJECTS_LIST[0].name;
   const p1_description = document.getElementById('p1-description');
@@ -93,7 +92,7 @@ SEE_PROJECT1.onclick = () => {
   const p1_lan3 = document.getElementById('p1-lan3');
   p1_lan3.innerHTML = PROJECTS_LIST[0].technologies[2];
 }
-SEE_PROJECT2.onclick = () => {
+SEE_PROJECT2.onclick = ()=> {
   const p2_name = document.getElementById('p2-name');
   p2_name.innerHTML = PROJECTS_LIST[1].name;
   const p2_description = document.getElementById('p2-description');
@@ -109,7 +108,7 @@ SEE_PROJECT2.onclick = () => {
   const p2_lan3 = document.getElementById('p2-lan3');
   p2_lan3.innerHTML = PROJECTS_LIST[1].technologies[2];
 }
-SEE_PROJECT3.onclick = () => {
+SEE_PROJECT3.onclick = ()=> {
   const p3_name = document.getElementById('p3-name');
   p3_name.innerHTML = PROJECTS_LIST[2].name;
   const p3_description = document.getElementById('p3-description');
@@ -125,7 +124,7 @@ SEE_PROJECT3.onclick = () => {
   const p3_lan3 = document.getElementById('p3-lan3');
   p3_lan3.innerHTML = PROJECTS_LIST[2].technologies[2];
 }
-SEE_PROJECT4.onclick = () => {
+SEE_PROJECT4.onclick = ()=> {
   const p4_name = document.getElementById('p4-name');
   p4_name.innerHTML = PROJECTS_LIST[3].name;
   const p4_description = document.getElementById('p4-description');
@@ -141,7 +140,6 @@ SEE_PROJECT4.onclick = () => {
   const p4_lan3 = document.getElementById('p4-lan3');
   p4_lan3.innerHTML = PROJECTS_LIST[3].technologies[2];
 }
-
 
 const PROJECTS_LIST_DESK = [{
   name: 'Tonic',
@@ -160,7 +158,7 @@ const PROJECTS_LIST_DESK = [{
   name: 'Facebook 360',
   description: '<li class="card-list-first-child card-list-item">FACEBOOK</li><li class="card-list-item">Full Stack Dev</li><li class="card-list-item">2018</li>',
   image: 'images/desktop-bg/desk-card-3.svg',
-  technologies: ['HTML', 'CSS', 'JavaScript', 'Runby on rails'],
+  technologies: ['HTML', 'CSS', 'JavaScript','Runby on rails'],
 },
 {
   name: 'Uber Navigation',
@@ -174,7 +172,7 @@ const SEE_DESK_PROJECT2 = document.getElementById('see-btn2-desk');
 const SEE_DESK_PROJECT3 = document.getElementById('see-btn3-desk');
 const SEE_DESK_PROJECT4 = document.getElementById('see-btn4-desk');
 
-SEE_DESK_PROJECT1.onclick = () => {
+SEE_DESK_PROJECT1.onclick = ()=> {
   const p1_name_d = document.getElementById('p1-name-d');
   p1_name_d.innerHTML = PROJECTS_LIST_DESK[0].name;
   const p1_description_d = document.getElementById('p1-description-d');
@@ -190,7 +188,7 @@ SEE_DESK_PROJECT1.onclick = () => {
   const p1_lan3_d = document.getElementById('p1-lan3-d');
   p1_lan3_d.innerHTML = PROJECTS_LIST_DESK[0].technologies[2];
 }
-SEE_DESK_PROJECT2.onclick = () => {
+SEE_DESK_PROJECT2.onclick = ()=> {
   const p2_name_d = document.getElementById('p2-name-d');
   p2_name_d.innerHTML = PROJECTS_LIST_DESK[1].name;
   const p2_description_d = document.getElementById('p2-description-d');
@@ -206,7 +204,7 @@ SEE_DESK_PROJECT2.onclick = () => {
   const p2_lan3_d = document.getElementById('p2-lan3-d');
   p2_lan3_d.innerHTML = PROJECTS_LIST_DESK[1].technologies[2];
 }
-SEE_DESK_PROJECT3.onclick = () => {
+SEE_DESK_PROJECT3.onclick = ()=> {
   const p3_name_d = document.getElementById('p3-name-d');
   p3_name_d.innerHTML = PROJECTS_LIST_DESK[2].name;
   const p3_description_d = document.getElementById('p3-description-d');
@@ -222,19 +220,19 @@ SEE_DESK_PROJECT3.onclick = () => {
   const p3_lan3_d = document.getElementById('p3-lan3-d');
   p3_lan3_d.innerHTML = PROJECTS_LIST_DESK[2].technologies[2];
 }
-// SEE_DESK_PROJECT4.onclick = ()=> {
-//   const p4_name_d = document.getElementById('p4-name-d');
-//   p4_name_d.innerHTML = PROJECTS_LIST_DESK[3].name;
-//   const p4_description_d = document.getElementById('p4-description-d');
-//   p4_description_d.innerHTML = PROJECTS_LIST_DESK[3].description;
-//   const p4_img_d = document.getElementById('p4-img-d');
-//   p4_img_d.src = PROJECTS_LIST_DESK[3].image;
-//   const p4_p_d = document.getElementById('p4-p-d');
-//   p4_p_d.innerHTML = PROJECTS_LIST_DESK[0].project_text_d;
-//   const p4_lan1_d = document.getElementById('p4-lan1-d');
-//   p4_lan1_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[0];
-//   const p4_lan2_d = document.getElementById('p4-lan2-d');
-//   p4_lan2_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[1];
-//   const p4_lan3_d = document.getElementById('p4-lan3-d');
-//   p4_lan3_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[2];
-// }
+SEE_DESK_PROJECT4.onclick = ()=> {
+  const p4_name_d = document.getElementById('p4-name-d');
+  p4_name_d.innerHTML = PROJECTS_LIST_DESK[3].name;
+  const p4_description_d = document.getElementById('p4-description-d');
+  p4_description_d.innerHTML = PROJECTS_LIST_DESK[3].description;
+  const p4_img_d = document.getElementById('p4-img-d');
+  p4_img_d.src = PROJECTS_LIST_DESK[3].image;
+  const p4_p_d = document.getElementById('p4-p-d');
+  p4_p_d.innerHTML = PROJECTS_LIST_DESK[0].project_text_d;
+  const p4_lan1_d = document.getElementById('p4-lan1-d');
+  p4_lan1_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[0];
+  const p4_lan2_d = document.getElementById('p4-lan2-d');
+  p4_lan2_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[1];
+  const p4_lan3_d = document.getElementById('p4-lan3-d');
+  p4_lan3_d.innerHTML = PROJECTS_LIST_DESK[3].technologies[2];
+}
